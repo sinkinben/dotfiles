673d22732958fee1f67f8bd2efba04004f49250f
sinkinben <sinkinben@outlook.com>
2023-05-10 17:01:15 +0800

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -8,32 +8,32 @@ function Write-BranchName () {
         if ($branch -eq "HEAD") {
             # we're probably in detached HEAD state, so print the SHA
             $branch = git rev-parse --short HEAD
-            Write-Host " ($branch)" -ForegroundColor "red"
+            Write-Host " ($branch)" -NoNewline -ForegroundColor "red"
         }
         else {
             # we're on an actual branch, so print it
-            Write-Host " ($branch)" -ForegroundColor "blue"
+            Write-Host " ($branch)" -NoNewline -ForegroundColor "blue"
         }
     } catch {
         # we'll end up here if we're in a newly initiated git repo
-        Write-Host " (no branches yet)" -ForegroundColor "yellow"
+        Write-Host " (no branches yet)" -NoNewline -ForegroundColor "yellow"
     }
 }
 
 function prompt {
     $base = "PS "
     $path = "$($executionContext.SessionState.Path.CurrentLocation)"
-    $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
+    $userPrompt = "$('' * ($nestedPromptLevel + 1)) "
 
     Write-Host "`n$base" -NoNewline
 
     if (Test-Path .git) {
-        Write-Host $path -NoNewline -ForegroundColor "green" -NoNewline
+        Write-Host $path -NoNewline -ForegroundColor "Magenta"
         Write-BranchName
     }
     else {
         # we're not in a repo so don't bother displaying branch name/sha
-        Write-Host $path -ForegroundColor "green"
+        Write-Host $path -NoNewline -ForegroundColor "Magenta"
     }
 
     return $userPrompt
